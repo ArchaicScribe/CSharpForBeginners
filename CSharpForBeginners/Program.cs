76cd6090c8a71a3886ac779847e25070abf489f4
Alex Rauenzahn <xandermv@yahoo.com>
2020-05-20 09:26:19 -0600

    Added in more practice.

--- CSharpForBeginners/Program.cs
+++ CSharpForBeginners/Program.cs
@@ -23,51 +23,74 @@ namespace CSharpFundamentals
             }
         }
 
-        public static int GetDimensions(string landscape, int portrait)
+       /* public static string GetDimensions(int landscape, int portrait)
         {
-            string ladnscapeValue = "You have chosen landscape.";
-            string portraitValue = "You have chosen portrait.";
-
             if (string.Equals(portrait, landscape))
             {
-                return int.Parse(ladnscapeValue);
-            }
-            else if (portrait > landscape)
-            {
-                return int.Parse(portraitValue);
+                Console.WriteLine($"{landscape} is greater than {portrait} you have chosen landscape.");
             }
             else
             {
-                throw new Exception("That is not a number");
+                Console.WriteLine($"{portrait} is greater than {landscape} you have chosen portrait.");
+            } else
+            {
+                return ("Please enter a new number");
             }
-        }
+        */
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number between 1 - 10.");
-            var userInput = Int32.Parse(Console.ReadLine());
-            if (userInput > 1 && userInput < 10)
+            //Console.WriteLine("Please enter a number between 1 - 10.");
+            //var userInput = Int32.Parse(Console.ReadLine());
+            //if (userInput > 1 && userInput < 10)
+            //{
+            //    Console.WriteLine("Valid!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Valid");
+            //}
+            //Console.WriteLine("Please enter your first number.");
+            //var firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter your second number.");
+            //var secondNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("The greatest of your two numbers are: " + GetMax(firstNumber, secondNumber));
+
+            //Console.WriteLine("Please enter a width.");
+            //var landscape = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter a height");
+            //var portrait = int.Parse(Console.ReadLine());
+
+            //var name = "John Smith";
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //Console.WriteLine("The dimensions you have chosen are "  + GetDimensions(landscape, portrait));
+
+
+            //var numbers = new int[] { 1, 2, 3, 4 };
+            //foreach(var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            var i = 0;
+            while(i <= 10)
             {
-                Console.WriteLine("Valid!");
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
             }
-            else
-            {
-                Console.WriteLine("Not Valid");
-            }
-            Console.WriteLine("Please enter your first number.");
-            var firstNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your second number.");
-            var secondNumber = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("The greatest of your two numbers are: " + GetMax(firstNumber, secondNumber));
-
-            Console.WriteLine("Please enter a width.");
-            var landscape = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter a height");
-            var portrait = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("The dimensions you have chosen are "  + GetDimensions(landscape, portrait));
         }
     }
 }
