e31f4b97bb60cb51c803cbae423151b2578e019d
Alex Rauenzahn <xandermv@yahoo.com>
2020-06-01 16:36:08 -0600

    Strings and their methods

--- CSharpForBeginners/Program.cs
+++ CSharpForBeginners/Program.cs
@@ -12,36 +12,38 @@ namespace CSharpFundamentals
 
         public static void Main(string[] args)
         {
-            //Creating timeSpan obejcts
-            var timeSpan = new TimeSpan(1, 2, 6);
+            var fullName = "Alex Rauenzahn  ";
+            Console.WriteLine("Trim: '{0}'" , fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'" , fullName.ToUpper().Trim());
+
+            var indexFN = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, indexFN);
+            var lastName = fullName.Substring(indexFN + 1);
+            Console.WriteLine("First name is: " + firstName);
+            Console.WriteLine("Last name is: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First name is: " + names[0]);
+            Console.WriteLine("Last name is: " + names[1]);
 
 
-            var timeSpan1 = new TimeSpan(1, 0, 0);
-            var timeSpan2 = TimeSpan.FromHours(1);
+            Console.WriteLine( fullName.Replace("Alex", "Alexander"));
+            fullName.Replace('e', 'E');
+            fullName.Replace(' ', 'E');
 
-            var start = DateTime.Now;
-            var end = DateTime.Now.AddMinutes(3);
-            var duration = start - end;
-            Console.WriteLine("Duration: " +  duration);
-
-
-            //Properties
-            Console.WriteLine("Minutes: " + timeSpan.Minutes);
-            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
-
-            //Add     
-            Console.WriteLine("Add example: " + timeSpan.Add(TimeSpan.FromMinutes(10)));
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
 
 
-            //Subrtract 
-            Console.WriteLine("Subtract example: " + timeSpan.Subtract(TimeSpan.FromHours(1)));
+            var userAge = "25";
+            var age = Convert.ToByte(userAge);
+            Console.WriteLine(age);
 
-            //ToString
-            Console.WriteLine("ToString " + timeSpan.ToString());
-
-            //Parse
-            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:34"));
-        
+            float price = 29.95f;
+            var fullprice = price.ToString("C0");
+            Console.WriteLine(fullprice);
         }
     }
 }
