25cbc14521795cfbc1016a0b5d865e44478f97a3
Alex Rauenzahn <xandermv@yahoo.com>
2020-06-02 11:17:03 -0600

    StringBuilder

--- CSharpForBeginners/Program.cs
+++ CSharpForBeginners/Program.cs
@@ -1,8 +1,6 @@
 ﻿using CSharpForBeginners;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
+using System.Text;
 
 namespace CSharpFundamentals
 {
@@ -13,11 +11,27 @@ namespace CSharpFundamentals
 
         public static void Main(string[] args)
         {
-            var sentence = "This is going to be a really really really really really really really long text";
-            var summary = StringUtility.SummerizeText(sentence);
-            Console.WriteLine(summary);
 
-                }
+            var builder = new StringBuilder("Hello, World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+            Console.WriteLine("First Char: " + builder[0]);
+
+
+
+            //var sentence = "This is going to be a really really really really really really really long text";
+            //var summary = StringUtility.SummerizeText(sentence);
+            //Console.WriteLine(summary);
+
+         }
         
     }
 }
