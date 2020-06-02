c0926a8c4e66cf8e1e94e64d00ac8f32927d9001
Alex Rauenzahn <xandermv@yahoo.com>
2020-06-02 10:41:56 -0600

    String Utility

--- CSharpForBeginners/Program.cs
+++ CSharpForBeginners/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpForBeginners;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -12,39 +13,12 @@ namespace CSharpFundamentals
 
         public static void Main(string[] args)
         {
-            var fullName = "Alex Rauenzahn  ";
-            Console.WriteLine("Trim: '{0}'" , fullName.Trim());
-            Console.WriteLine("ToUpper: '{0}'" , fullName.ToUpper().Trim());
+            var sentence = "This is going to be a really really really really really really really long text";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
 
-            var indexFN = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, indexFN);
-            var lastName = fullName.Substring(indexFN + 1);
-            Console.WriteLine("First name is: " + firstName);
-            Console.WriteLine("Last name is: " + lastName);
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("First name is: " + names[0]);
-            Console.WriteLine("Last name is: " + names[1]);
-
-
-            Console.WriteLine( fullName.Replace("Alex", "Alexander"));
-            fullName.Replace('e', 'E');
-            fullName.Replace(' ', 'E');
-
-            if (String.IsNullOrWhiteSpace(" "))
-            {
-                Console.WriteLine("Invalid");
-            }
-
-
-            var userAge = "25";
-            var age = Convert.ToByte(userAge);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            var fullprice = price.ToString("C0");
-            Console.WriteLine(fullprice);
-        }
+                }
+        
     }
 }
 
