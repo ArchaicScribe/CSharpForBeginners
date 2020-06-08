47d16f63f5aff1921b469874451c3a75a3f8cf57
Alex Rauenzahn <xandermv@yahoo.com>
2020-06-08 10:31:37 -0600

    List of unique Int numbers. Need to add validation, and change to inform user of restrictions.

--- CSharpForBeginners/Program.cs
+++ CSharpForBeginners/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpForBeginners;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace CSharpFundamentals
@@ -11,20 +12,27 @@ namespace CSharpFundamentals
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("What is your name? ");
-            var userName = Console.ReadLine();
-            var reversedName = NameReversed(userName);
-            Console.WriteLine("Your name reversed is: " + reversedName);
-        }
-
-        public static string NameReversed(string userName)
-        {
-            var nameArray = new char[userName.Length];
-            for (var i = userName.Length; i > 0; i--)
+            var numbers = new List<int>();
+            while (true)
             {
-                nameArray[userName.Length - 1] = userName[i - 1];
+                Console.WriteLine("Please enter a number (or 'Quit' to exit): ");
+                var userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "quit")
+                    break;
+                numbers.Add(Convert.ToInt32(userInput));
             }
-           return new string(nameArray);
+            var uniques = new List<int>();
+            foreach(var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+            Console.WriteLine("Unique numbers:");
+            foreach(var number in uniques)
+            {
+                Console.WriteLine(number);
+            }
         }
         
     }
