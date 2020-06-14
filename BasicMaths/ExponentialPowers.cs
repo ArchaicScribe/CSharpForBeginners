0434656a2e41385d294f5a5884e2b7b863c8d0a0
Alex Rauenzahn <xandermv@yahoo.com>
2020-06-14 12:20:16 -0600

    Exponential powers

---
+++ BasicMaths/ExponentialPowers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicMaths
+{
+    public class ExponentialPowers
+    {
+        public static double ExponentialPower(double baseNumber, double exponent)
+        {
+            double result = Math.Pow(baseNumber, exponent);
+
+            return result;
+
+
+        }
+    }
+}
--- BasicMaths/Program.cs
+++ BasicMaths/Program.cs
@@ -1,29 +1,40 @@
 ﻿using System;
 
+
 namespace BasicMaths
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var setValue = 63;
             Random random = new Random();
             int randomValue = random.Next(1,100);
             var players = 4;
             var offset = 1;
+            ExponentialPower(2, 24);
             
 
             Console.WriteLine("The set value is " + setValue);
             Console.WriteLine("The starting player is: " + SelectStartingPlayer(setValue, players, offset) + " Congrats.");
             Console.WriteLine("The next player, selected at random is: " + randomValue);
             Console.WriteLine("That player is player: " + SelectStartingPlayer(setValue, players, offset) + " Congrats.");
-
+            Console.WriteLine(ExponentialPower(2, 24));
         }
         private static int SelectStartingPlayer(int setValue, int players, int offset)
         {
             return (setValue % players) + offset;
         }
 
-       
+        public static double ExponentialPower(double baseNumber, double exponent)
+        {
+            double result = Math.Pow(baseNumber, exponent);
+
+            return result;
+
+
+        }
+
+
     }
 }
